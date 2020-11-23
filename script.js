2c916ed703b09281a8c6196b4f9fa5fb6ac38ece
Sander van Zaane <mail@sandervanzaane.nl>
2020-11-23 15:25:55 +0100

    add favorite movie

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const clientCall = async() => {
     data = await getData();
-    console.log(data);
-}
+    const genre = data.genres;
 
+    for (let i = 0; i < genre.length; i++) {
+
+        const ul = document.getElementById("complete-list");
+        const li = document.createElement("li");
+        const textHolder = document.createTextNode(`${genre[i].id} ${genre[i].name}`);
+        ul.appendChild(li);
+        li.appendChild(textHolder);
+        console.log(genre[i]);
+        console.log("loopje werkt! ")
+    };
+}
 clientCall();
+
+// favorite movie
+const ul = document.getElementById("favorite");
+const li = document.createElement("li");
+const textHolder = document.createTextNode(`${genre[i].id} ${genre[i].name}`);
+ul.appendChild(li);
+li.appendChild(textHolder);
+console.log(genre[i]);
+
+https: //api.themoviedb.org/3/find/tt6105098?api_key=e8c91e364927d6dd958e5fe7eabb7541&language=en-US&external_source=imdb_id
